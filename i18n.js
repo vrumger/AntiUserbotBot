a2873bc444ad6a906a75adc85210b23234955fb3
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-02-18 10:36:48 -0500

    Bug fix (thanks https://t.me/gabriel)

--- i18n.js
+++ i18n.js
@@ -3,7 +3,7 @@ const languages = process.env.I18N.split(`,`);
 const i18n = newI18n(`${__dirname}/i18n`, languages);
 
 module.exports = function (keyword, variables) {
-    let [lang] = this.from.language_code.split('-');
+    let [lang] = (this.from.language_code || ``).split('-');
     lang = i18n.languages.includes(lang) ? lang : `en`;
 
     return i18n(lang, keyword, variables);
