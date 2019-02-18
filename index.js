b36ad7c0f94f70c986471af97704b7c2d7d7812c
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-02-18 11:51:22 -0500

    Add "I'm not a bot" to i18n

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.context.keyboard = function () {
     const { id } = this.message.new_chat_member;
 
     return Markup.inlineKeyboard([
-        Markup.callbackButton(`I'm not a bot`, `unmute.${id}`)
+        Markup.callbackButton(this.i18n(`not_a_bot`), `unmute.${id}`)
     ]).extra();
 }
 
